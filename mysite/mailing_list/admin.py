09bb28459999e8e5a6f63ce8837d20c0df8139bd
Eric Barbour <emb4gu@virginia.edu>
2015-09-10 10:18:52 -0400

    Code cleanup and api function format for Groups All of the POST, PATCH, and DELETE calls to the google groups api do not throw errors. They also all use the service method instead of the raw http request. These changes are still not showing in the admin console. This needs to be investigated.

--- mysite/mailing_list/admin.py
+++ mysite/mailing_list/admin.py
@@ -6,9 +6,6 @@ from .models import MailingList
 class MailingListAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
-    def add(self, request):
-        pass
-
     def get_form(self, request, obj=None, **kwargs):
         form = super(MailingListAdmin, self).get_form(request, obj, **kwargs)
         form.current_user = request.user
