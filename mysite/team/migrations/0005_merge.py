0bcef314007b2064b1f4351ef2bca9f99c806cca
Chelsea Overstreet <overstreetchelsea@gmail.com>
2015-10-06 15:18:45 -0400

    Exporting work

---
+++ mysite/team/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('team', '0004_auto_20151006_1405'),
+        ('team', '0003_auto_20151006_1325'),
+    ]
+
+    operations = [
+    ]
