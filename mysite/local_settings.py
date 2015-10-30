a84eb28007af9e34ebce339af9693bc736b74eaa
Zobair Alijan <zobair@Zobairs-MacBook-Air.local>
2015-10-30 16:01:24 -0400

    update

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 
 DEBUG = True
 
@@ -21,3 +23,18 @@ DATABASES = {
         "PORT": "",
     }
 }
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTOCOL", "https")
+
+CACHE_MIDDLEWARE_SECONDS = 60
+
+CACHE_MIDDLEWARE_KEY_PREFIX = ''
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
+        "LOCATION": "127.0.0.1:11211",
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
