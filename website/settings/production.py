de6071dd5db930459f5d3556a8aa34b8d3695601
Joshua Bird <admin@Orion.local>
2016-09-11 22:06:35 -0400

    Serving static files

--- website/settings/production.py
+++ website/settings/production.py
@@ -9,6 +9,18 @@ DATABASES['default'] = dj_database_url.config()
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['cosio.herokuapp.com']
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 
 try:
     from .local import *
