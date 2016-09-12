e2468ef5810ab2b4b218cbf1b2df484611b3a838
Joshua Bird <admin@Orion.local>
2016-09-11 23:07:18 -0400

    Setting DEBUG = True

--- website/settings/production.py
+++ website/settings/production.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import dj_database_url
 from .base import *
 
-DEBUG = False
+#DEBUG = False
 DATABASES['default'] = dj_database_url.config()
 
 SECRET_KEY = os.environ['SECRET_KEY']
