0bcabefca3fecbc6739578e11cd77956796b8d90
Joshua Bird <admin@Orion.local>
2016-09-11 22:30:21 -0400

    use github to serve static files

--- website/settings/base.py
+++ website/settings/base.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'https://cdn.rawgit.com/HalcyonChimera/cos.io/master/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
