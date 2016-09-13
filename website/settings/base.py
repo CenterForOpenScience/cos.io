37e6712fb5a4b2c71536a287a5fba046d0735d32
Joshua Bird <admin@Orion.local>
2016-09-12 20:28:55 -0400

    Use S3 for staticfiles and media storage

--- website/settings/base.py
+++ website/settings/base.py
@@ -16,11 +16,6 @@ import os
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
-
 # Application definition
 
 INSTALLED_APPS = [
--- website/settings/production.py
+++ website/settings/production.py
@@ -12,17 +12,20 @@ DATABASES['default'] = dj_database_url.config()
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ['cosio.herokuapp.com']
+#ALLOWED_HOSTS = ['cosio.herokuapp.com']
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = 'https://rawgit.com/HalcyonChimera/cos.io/master/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, 'static'),
+]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -30,12 +33,17 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'cos.io'
+AWS_STORAGE_BUCKET_NAME = 'cosio'
+AWS_QUERYSTRING_AUTH = False
 
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'website.s3utils.S3BotoStorage'
+STATICFILES_STORAGE = 'website.s3utils.S3BotoStorage'
 
-STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+STATIC_DIRECTORY = 'static/'
+MEDIA_DIRECTORY = 'media/'
+
+STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/' + STATIC_DIRECTORY
+MEDIA_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/' + MEDIA_DIRECTORY
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 try:
