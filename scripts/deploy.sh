84938e26665d1576e744d59b4e45cfec14fc9665
Joshua Bird <admin@Orion.local>
2016-10-06 04:45:01 -0400

    garbage

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,20 @@
 #! /bin/sh
 
-echo $TESTKEY > ~/.ssh/id_rsa.pub
+echo $SSH_KEY > ~/.ssh/id_rsa.pub
 curl -sSL http://deis.io/deis-cli/install-v2.sh | bash
-./deis keys:add ~/.ssh/id_rsa.pub
+deis login https://deis.mechanysm.com/ --username $DEIS_USERNAME --password $DEIS_PASSWORD
+deis config:set \
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    AWS_SECRET_ACCESS_KEY=$AWS_SEVRET_ACCESS_KEY \
+    DATABASE_ENGINE=$DATABASE_ENGINE \
+    DATABASE_HOST=$DATABASE_HOST \
+    DATABASE_NAME=$DATABASE_NAME \
+    DATABASE_PASSWORD=$DATABASE_PASSWORD \
+    DATABASE_PORT=$DATABASE_PORT \
+    DATABASE_USER=$DATABASE_USER \
+    DEIS=True \
+    DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE \
+    SECRET_KEY=$DJANGO_SETTINGS_MODULE &>/dev/null
 git push deis master
 return 1
 
