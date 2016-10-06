e0c56f1cf4b03809a21239f69b4e2d03e419b685
Joshua Bird <admin@Orion.local>
2016-10-06 16:32:33 -0400

    ssh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,12 @@
 curl -sSL http://deis.io/deis-cli/install-v2.sh | bash
 
 yes | ./deis login https://deis.mechanysm.com/ --username $DEIS_USERNAME --password $DEIS_PASSWORD
+eval "$(ssh-agent -s)"
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_deis > /dev/null
-chmod go-w ~/
+ssh-keygen -f ~/.ssh/id_deis.pub -e -m pem
 chmod 700 ~/.ssh
-chmod 600 ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/id_deis.pem
+ssh-add ~/.sshi/id_deis.pem
 ./deis keys:add ~/.ssh/id_deis.pub
 ./deis git:remote -a $DEIS_APP_NAME
 ./deis config:set \
