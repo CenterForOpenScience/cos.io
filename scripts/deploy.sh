4c794e5bc539e67d52bde365ed89a9d1bdc6dc88
Joshua Bird <admin@Orion.local>
2016-10-06 14:19:28 -0400

    script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,30 @@
 #! /bin/sh
+
 echo $SSH_KEY > ~/.ssh/id_rsa.pub
-echo '${USER}@${HOST}'
+echo "$(whoami)@$(hostname)"
+echo "$(hostname --long)"
+
 curl -sSL http://deis.io/deis-cli/install-v2.sh | bash
-./deis login https://deis.mechanysm.com/ --username $DEIS_USERNAME --password $DEIS_PASSWORD
-./deis git:remote -a $DEIS_APP_NAME
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_deis
-./deis keys:add ~/.ssh/id_deis.pub
-./deis config:set \
-    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-    AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-    DATABASE_ENGINE=$DATABASE_ENGINE \
-    DATABASE_HOST=$DATABASE_HOST \
-    DATABASE_NAME=$DATABASE_NAME \
-    DATABASE_PASSWORD=$DATABASE_PASSWORD \
-    DATABASE_PORT=$DATABASE_PORT \
-    DATABASE_USER=$DATABASE_USER \
-    DEIS=True \
-    DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE \
-    SECRET_KEY=$DJANGO_SETTINGS_MODULE &>/dev/null
-git push deis master
+
+
+{
+    ./deis login https://deis.mechanysm.com/ --username $DEIS_USERNAME --password $DEIS_PASSWORD
+    ./deis git:remote -a $DEIS_APP_NAME
+    ssh-keygen -t rsa -N "" -f ~/.ssh/id_deis
+    ./deis keys:add ~/.ssh/id_deis.pub
+    ./deis config:set \
+        AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+        AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+        DATABASE_ENGINE=$DATABASE_ENGINE \
+        DATABASE_HOST=$DATABASE_HOST \
+        DATABASE_NAME=$DATABASE_NAME \
+        DATABASE_PASSWORD=$DATABASE_PASSWORD \
+        DATABASE_PORT=$DATABASE_PORT \
+        DATABASE_USER=$DATABASE_USER \
+        DEIS=True \
+        DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE \
+        SECRET_KEY=$DJANGO_SETTINGS_MODULE 
+    #git push deis master
+} &> /dev/null
+
+
