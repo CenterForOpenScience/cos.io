bb9ae8be3148d2c2f739a76f3491d1d1e00d00f6
Joshua Bird <admin@Orion.local>
2016-10-06 05:33:57 -0400

    oops

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
 #! /bin/sh
-
 echo $SSH_KEY > ~/.ssh/id_rsa.pub
 curl -sSL http://deis.io/deis-cli/install-v2.sh | bash
-deis login https://deis.mechanysm.com/ --username $DEIS_USERNAME --password $DEIS_PASSWORD
-deis config:set \
+./deis login https://deis.mechanysm.com/ --username $DEIS_USERNAME --password $DEIS_PASSWORD
+./deis config:set \
     AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     AWS_SECRET_ACCESS_KEY=$AWS_SEVRET_ACCESS_KEY \
     DATABASE_ENGINE=$DATABASE_ENGINE \
@@ -16,5 +15,3 @@ deis config:set \
     DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE \
     SECRET_KEY=$DJANGO_SETTINGS_MODULE &>/dev/null
 git push deis master
-return 1
-
