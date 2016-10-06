080c299e8d4a1061821f1a53f091f425d479b87c
Joshua Bird <admin@Orion.local>
2016-10-06 04:16:22 -0400

    deis cli tools

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
-git deis push master
+$TESTKEY > ~/.ssh/id_rsa.pub
+curl -sSL http://deis.io/deis-cli/install-v2.sh | bash
+./deis keys:add ~/.ssh/id_rsa.pub
+git push deis master
 return 1
 
