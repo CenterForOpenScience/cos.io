a9a75cb6c95d5fbb136e13b44aeff82cf057a4bd
Joshua Bird <admin@Orion.local>
2016-10-06 17:29:58 -0400

    debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,22 +4,27 @@ curl -sSL http://deis.io/deis-cli/install-v2.sh | bash
 
 yes | ./deis login https://deis.mechanysm.com/ --username $DEIS_USERNAME --password $DEIS_PASSWORD
 eval "$(ssh-agent -s)"
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_deis > /dev/null
-ls -la ~/.ssh/
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/id_deis
-chmod 600 ~/.ssh/id_deis.pub
+ssh-keygen -t rsa -N "" -f .travis/id_deis > /dev/null
+chmod 600 .travis/id_deis
+chmod 600 .travis/id_deis.pub
 
 expect <<LFDS
-spawn ssh-add ~/.ssh/id_deis
-expect "Enter passphrase for /home/travis/.ssh/id_deis:"
+spawn ssh-add .travis/id_deis
+expect "Enter passphrase for /home/travis/.travis/id_deis:"
+send "";
+expect eof
+LFDS
+
+expect <<LFDS
+spawn ssh-add .travis/id_deis.pub
+expect "Enter passphrase for /home/travis/.travis/id_deis.pub:"
 send "";
 expect eof
 LFDS
 
 ./deis git:remote -a $DEIS_APP_NAME
-./deis keys:add ~/.ssh/id_deis.pub
-cat .git//config
+./deis keys:add .travis.id_deis.pub
+cat .git/config
 ./deis config:set \
     AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
