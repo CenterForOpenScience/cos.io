520d4cea9dd700c59585e72e37e2c8b9aa4b0f14
Joshua Bird <joshua.thomas.bird@gmail.com>
2016-10-12 10:32:28 -0400

    addpadding block

--- common/blocks/clearfix.py
+++ common/blocks/clearfix.py
@@ -6,6 +6,8 @@ A block to provide a clearfix element for Wagtail's StreamField.
 
 
 from wagtail.wagtailcore import blocks
+from django.db.models import IntegerField
+
 
 class ClearfixBlock(blocks.StructBlock):
     class Meta:
@@ -14,3 +16,17 @@ class ClearfixBlock(blocks.StructBlock):
         label = 'Clearfix Block'
         help_text = ('When you need to make sure that your next element(s) is on a new line from '
                      'the previous elements, use this little helper block.')
+
+class ClearfixBlock(blocks.StructBlock):
+
+    height = IntegerField()
+
+
+
+    class Meta:
+        template = 'common/blocks/whitespace_block.html'
+        icon = 'placeholder'
+        label = 'Whitespace Block'
+        help_text = ('When you need to make sure that your next element(s) is on a new line from '
+                     'the previous elements, use this little helper block.')
+
