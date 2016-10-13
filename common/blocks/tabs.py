242105d9fd2a77cb9187ca167f23d5e4b7cb0a60
Joshua Bird <joshua.thomas.bird@gmail.com>
2016-10-13 01:13:10 -0400

    use simple counter

--- common/blocks/tabs.py
+++ common/blocks/tabs.py
@@ -25,16 +25,16 @@ class TabBlock(StructBlock):
     name = CharBlock()
     content = GenericContentStreamBlock(local_blocks=[('columns', ColumnsBlock())])
 
-    def get_prep_value(self, value):
-        value.pop('css_name', None)
-        value = super(TabBlock, self).get_prep_value(value)
-        return
-        
+    #def get_prep_value(self, value):
+    #    value.pop('css_name', None)
+    #    value = super(TabBlock, self).get_prep_value(value)
+    #    return
+    #    
 
-    def to_python(self, value):
-        value = super(TabBlock, self).to_python(value)
-        value['css_name'] = randomword(25)
-        return value
+    #def to_python(self, value):
+    #    value = super(TabBlock, self).to_python(value)
+    #    value['css_name'] = randomword(25)
+    #    return value
 
     #@property
     #def css_name(self):
