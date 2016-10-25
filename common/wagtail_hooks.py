f864fedf844701ee8ed0bb69dc78d6cfc0eb300e
Joshua Bird <joshua.thomas.bird@gmail.com>
2016-10-25 16:36:02 -0400

    remove fontawesome stylesheet

--- common/wagtail_hooks.py
+++ common/wagtail_hooks.py
@@ -6,12 +6,12 @@ from django.utils.html import format_html
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from wagtail.wagtailcore import hooks
 
-@hooks.register('insert_global_admin_css')
-def global_admin_css():
-    return format_html(
-        '<link rel="stylesheet" href="{}">',
-        static('plugins/font-awesome/css/font-awesome.min.css')
-    )
+#@hooks.register('insert_global_admin_css')
+#def global_admin_css():
+#    return format_html(
+#        '<link rel="stylesheet" href="{}">',
+#        static('plugins/font-awesome/css/font-awesome.min.css')
+#    )
 
 class PersonModelAdmin(ModelAdmin):
     model = Person
