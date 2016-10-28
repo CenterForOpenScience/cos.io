aefef16c90da9fe751f1b9035a8da7338110edd0
545zhou <545zhou@gmail.com>
2016-10-28 16:48:51 -0400

    able to toggle style div

--- common/blocks/codes.py
+++ common/blocks/codes.py
@@ -3,10 +3,12 @@ from common.blocks import LANGUAGE_CHOICES
 
 class CodeBlock(blocks.StructBlock):
 
+    css_style = blocks.CharBlock(required=False)
     language = blocks.ChoiceBlock(choices=LANGUAGE_CHOICES, default="python")
     codes = blocks.TextBlock()
 
     class Meta:
+        form_template = 'common/block_forms/code_block.html'
         template = 'common/blocks/code_block.html'
         icon = 'edit'
         label = 'Codes'
