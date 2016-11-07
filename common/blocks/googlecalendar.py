f9dff337a44c0bc7ba783f4bd057d640ccda1d13
545zhou <545zhou@gmail.com>
2016-11-07 15:44:00 -0500

    made calendar show right in phone view

--- common/blocks/googlecalendar.py
+++ common/blocks/googlecalendar.py
@@ -2,7 +2,7 @@ from wagtail.wagtailcore import blocks
 from .StructBlockWithStyle import StructBlockWithStyle
 
 class GoogleCalendarBlock(StructBlockWithStyle):
-    source = blocks.CharBlock(required=True,max_length=255,help_text="Such as: calendar@cos.io")
+    source = blocks.CharBlock(required=True,max_length=255,help_text="Such as: calendar@cos.io. You can also change the style of calendar block.")
 
     class Meta:
         template = 'common/blocks/google_calendar.html'
