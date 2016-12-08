c8cc8347163547f7999bf55efbea37f4acaa40c6
Joshua Bird <joshua.thomas.bird@gmail.com>
2016-12-08 13:52:17 -0500

    add wide image hero cap to header

--- common/blocks/hero.py
+++ common/blocks/hero.py
@@ -4,7 +4,12 @@ from .StructBlockWithStyle import StructBlockWithStyle
 
 class HeroBlock(StructBlockWithStyle):
     image = ImageChooserBlock(required=True)
-    description = blocks.RichTextBlock(required=True)
+    description = blocks.RawHTMLBlock(required=True)
+    image_display_setting = blocks.ChoiceBlock(choices=[
+        ('background', 'Cover the whole Hero as a background'),
+        ('icon', 'Center the image in the middle of the hero block')
+    ])
+    text_color = blocks.CharBlock(help_text='Enter a color for the text.')
 
     class Meta:
         template = 'common/blocks/hero_block.html'
