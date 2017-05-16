df018c5d59a2e3ed807805fbb66bcbce12756748
Baylee Swenson <baylee@cos.io>
2017-05-16 15:53:46 -0400

    blog search indexes

---
+++ cos_tests/blog_tests/test_blog_search_indexes.py
@@ -0,0 +1,19 @@
+import pytest
+
+from django.test import TestCase
+
+import blog.search_indexes as search_indexes
+from blog.models import BlogPage
+
+class BlogPageIndexTest(TestCase):
+
+    def setUp(self):
+        """ Sets up variables """
+        super(BlogPageIndexTest, self).setUp()
+        self.blog = search_indexes.BlogPageIndex
+
+    def test_blog_index(self):
+        """ Checks the blog index being returned """
+        blog_index = self.blog.get_model(self.blog)
+
+        self.assertEqual(blog_index, BlogPage, 'the results returned were not the same')
