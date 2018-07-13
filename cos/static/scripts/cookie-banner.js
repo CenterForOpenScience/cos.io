7441da0d84fc81448f0bf02cf55e52ad30d5bc5e
erinspace <erin.braswell@gmail.com>
2018-07-13 12:17:36 -0400

    Set value for cookie, remove unused var

--- cos/static/scripts/cookie-banner.js
+++ cos/static/scripts/cookie-banner.js
@@ -4,16 +4,16 @@ $(document).ready(function () {
     var cookieAcceptedKey = 'cos_cookieconsent';
     var daysToExpire = 30;
 
-    var $cookieBanner = cookieBannerElem.on('closed.bs.alert', function() {
+    cookieBannerElem.on('closed.bs.alert', function() {
         var d = new Date();
         d.setTime(d.getTime() + (daysToExpire*24*60*60*1000));
         var expires = "expires="+ d.toUTCString();
-        document.cookie = cookieAcceptedKey + "=;" + expires + ";path=/";
+        document.cookie = cookieAcceptedKey + "=1;" + expires + ";path=/";
         cookieBannerElem.hide();
     });
 
     var setCookies = document.cookie.split(';').map(function(cookie) {return cookie.trim();});
-    var accepted = $.inArray(cookieAcceptedKey + '=', setCookies);
+    var accepted = $.inArray(cookieAcceptedKey + '=1', setCookies);
     if (accepted === -1) {
         cookieBannerElem.css({'display': 'flex'});
         cookieBannerElem.show();
